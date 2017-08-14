0021a57a55c95c0d4304ac92aaac4c3145c20907
YARA SAGAR <yarasagar01@gmail.com>
2017-08-14 13:24:27 -0700

    Add files via upload
    Select query output is displayed in the table is the main theme. But till now i stored in vector of vector data type.

---
+++ JdbcStatement.java
@@ -0,0 +1,114 @@
+package dummy;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+public class JdbcStatement {
+
+	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/TEST";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "sagar225";
+
+	public static void main(String[] argv) {
+
+		try {
+
+			selectRecordsFromDbUserTable();
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+	private static void selectRecordsFromDbUserTable() throws SQLException {
+
+		Connection dbConnection = null;
+		Statement statement = null;
+
+		String selectTableSQL = "SELECT name, value from dummy";
+
+		try {
+			dbConnection = getDBConnection();
+			statement = dbConnection.createStatement();
+
+			System.out.println(selectTableSQL);
+
+			// execute select SQL stetement
+			ResultSet rs = statement.executeQuery(selectTableSQL);
+			ResultSetMetaData metaData = rs.getMetaData();
+
+		    // names of columns
+		    Vector<String> columnNames = new Vector<String>();
+		    int columnCount = metaData.getColumnCount();
+		    for (int column = 1; column <= columnCount; column++) {
+		        columnNames.add(metaData.getColumnName(column));
+		    }
+		    System.out.println(columnCount);
+		    System.out.println(columnNames.lastElement());
+		    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		    while (rs.next()) {
+		        Vector<Object> vector = new Vector<Object>();
+		        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+		            vector.add(rs.getObject(columnIndex));
+		        }
+		        data.add(vector);
+		    }
+		    System.out.println(data);
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (statement != null) {
+				statement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+
+	}
+
+	private static Connection getDBConnection() {
+
+		Connection dbConnection = null;
+
+		try {
+
+			Class.forName(DB_DRIVER);
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		try {
+
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+					DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		return dbConnection;
+
+	}
+
+}
